ac9d5bddde2e889fb219931d45f363a245dc9386
Priyanshu Gupta <priyanshugpt486@gmail.com>
2024-02-01 20:43:21 +0530

    added port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
-const app = express()
+import dotenv from "dotenv";
+dotenv.config()
 
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 app.get('/', (_,res)=>{
     res.send("Working !!")
 })
@@ -10,7 +12,7 @@ app.get('/new', (_,res)=>{
     res.send("New endpoint...")
 })
 
-app.listen(3001, ()=>{
+app.listen(PORT, ()=>{
     console.log("Server running on port 3001")
 })
 
