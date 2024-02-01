fc8b550661793e6c0d3cd26fe68bda04b71f91fd
Priyanshu Gupta <priyanshugpt486@gmail.com>
2024-02-01 20:45:37 +0530

    added port

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (_,res)=>{
     res.send("Working !!")
 })
-
 app.get('/new', (_,res)=>{
     res.send("New endpoint...")
 })
 
 app.listen(PORT, ()=>{
-    console.log("Server running on port 3001")
+    console.log("Server running on port ", PORT)
 })
 
