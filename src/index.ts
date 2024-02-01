021db49c705cf696d503c69de5e24e72444f8d90
Priyanshu Gupta <priyanshugpt486@gmail.com>
2024-02-01 20:20:36 +0530

    added about

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get('/', (_,res)=>{
     res.send("Working !!")
 })
 
+app.get('/about', (_,res)=>{
+    res.send("About endpoint...")
+})
+
 app.listen(3001, ()=>{
     console.log("Server running on port 3001")
 })
